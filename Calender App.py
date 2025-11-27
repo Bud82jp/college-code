f0eeffc1a8487f28e11581faa22bbae755c81a25
Bud82jp <JPretty@theprettys.co.uk>
2025-11-27 10:09:30 +0000

    Update Calender App.py

--- Calender App.py
+++ Calender App.py
@@ -1,24 +1,38 @@
-class Day:
+class Day: 
     positionInYear = 0
     event = ""
-    def __init__(self, positionInYear, eventname):
+    def __init__(self, positionInYear, event):
         self.positionInYear = positionInYear
-        self.eventname = eventname
-
-class Birthday(Day):
+        self.event = event
+ 
+class BirthDay(Day):
     age = 0
-    def __init__(self,positionInYear, event, age):
-        super().__init__(positionInYear, event)
+    def __init__(self,positionInYear,event,age):
+        super().__init__(positionInYear,event)
         self.age = age
-
-
-class Calendar():
+ 
+class BankHoliday(Day):
+    def __init__(self,positionInYear):
+        super().__init__(positionInYear,"Bank Holiday")
+ 
+class Calendar:
     days = []
-
-    def add_event(self, day, event):
-        self.days.append(Day(daynumber, eventname))
-    
-
-
-c = Calendar
+    def __init__(self):
+        self.days = []
+    def add_event(self,daynumber,eventname):
+        self.days.append(Day(daynumber,eventname))
+    def add_birthday(self,daynumber,age):
+        self.days.append(BirthDay(daynumber,"Birthday",age))
+    def add_bank_hol(self,daynumber):
+        self.days.append(BankHoliday(daynumber))
+ 
+c = Calendar()
 c.add_event(360, "Christmas")
+c.add_event(1, "New Year's")
+c.add_birthday(20,30)
+c.add_bank_hol(5)
+c.add_event(365, "New Year's Eve")
+c.add_event(361, "Boxing Day")
+ 
+for day in c.days:
+    print(f"The {day.positionInYear}th day of the year is {day.event}")
